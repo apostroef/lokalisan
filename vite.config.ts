f4f763d8e49f2b484fbea6b83fd65dcad3a3ce27
apostroef <prithaliacardions@gmail.com>
2026-03-27 23:21:47 +0700

    vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
-    base: process.env.VITE_BASE_PATH || "/",
+    base: process.env.VITE_BASE_PATH || "/lokalisan",
     esbuild: {
         jsx: 'automatic',
     },
