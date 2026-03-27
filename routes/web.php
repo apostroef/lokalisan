8ca6aea6be6ff045bf652712c1b3c70292ce1cda
apostroef <prithaliacardions@gmail.com>
2026-03-27 23:35:06 +0700

    route fix

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,27 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('homepage');
+    return Inertia::render('homepage.tsx');
 })->name('homepage');
 
 Route::get('/experience', function () {
-    return Inertia::render('experience');
+    return Inertia::render('experience.tsx');
 })->name('experience');
 
 Route::get('/rwfd', function () {
-    return Inertia::render('projects/Rwfd');
+    return Inertia::render('projects/Rwfd.tsx');
 })->name('rwfd');
 
 Route::get('/tedx-ua', function () {
-    return Inertia::render('projects/TEDX');
+    return Inertia::render('projects/TEDX.tsx');
 })->name('tedx-ua');
 
 Route::get('/beyond-the-bite', function () {
-    return Inertia::render('projects/BeyondBite');
+    return Inertia::render('projects/BeyondBite.tsx');
 })->name('beyond-the-bite');
 
 Route::get('/gallery', function () {
-    return Inertia::render('gallery');
+    return Inertia::render('gallery.tsx');
 })->name('gallery');
 
 Route::middleware(['auth', 'verified'])->group(function () {
